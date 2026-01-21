d492dd19e012b0b0bc9f3729834d6a1a85e35094
andrealezy <andrealezy1@gmail.com>
2026-01-21 10:05:29 -0300

    Primeiro Commit do projeto E-commerce Demo criado em 04/2024

---
+++ js/index.js
@@ -0,0 +1,12 @@
+console.log('%cALEZYSOLUTION', 'font-size: 24px; font-weight: bold; color: #00f0ff; text-shadow: 0 0 10px #00f0ff;');
+
+
+
+function newYear(){
+  const year = document.getElementById('ano');
+  year.textContent = new Date();
+}
+//
+let totalSegundos = [
+  {dias: 24 * 60 * 60}
+]
