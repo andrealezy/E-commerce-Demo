e1e48780be6f7d364bd9ecc451f7f7e04e7568d3
andrealezy <andrealezy1@gmail.com>
2026-01-21 10:47:42 -0300

    apenas ajsute de indentação no index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 console.log('%cALEZYSOLUTION', 'font-size: 24px; font-weight: bold; color: #00f0ff; text-shadow: 0 0 10px #00f0ff;');
 
-
-
 function newYear(){
   const year = document.getElementById('ano');
   year.textContent = new Date();
